dd34c2d8761ffac5671740d5f42f157db52188a7
Jurij <yurij-bukatkin@yandex.ru>
2014-03-31 07:49:59 +0400

    Update Twittya.py

--- Twittya.py
+++ Twittya.py
@@ -8,43 +8,59 @@ from BeautifulSoup import BeautifulSoup
 
 URL = "http://www.kolomna-kgpi.ru/index.php?option=com_content&task=blogcategory&id=1&Itemid=79"
 
+
 class Tweetya(object):
 
 	def __init__(self):
-		CONSUMER_KEY = ""
-		CONSUMER_SECRET = ""
-		ACCESS_KEY = ""
-		ACCESS_SECRET = ""
-		URL = ""
-		FILENAME = "tweets.txt"
+		return
+		
 
-	def auth():
+	def auth(self):
 		auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 		auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 		api = tweepy.API(auth)
+		api.update_status('Hello from tweepy!')
 		return api
 	
 	def getlinks(self):
+		links=[]
 		try:
 			handle = open(FILENAME,"r")
 			for line in handle.readlines():
 				links.append(line)
 		except:
 			print "Error read file"
-		fi.close
+		handle.close
 		return links
 
 	def setlink(self,link):
-		handle = open(FILENAME,'a')
-		handle.write(link+'\n');
-		handle.close
+		try:
+			handle = open(FILENAME,'a')
+			handle.write(link+'\n');
+			handle.close
+		except:
+			print "Error read file"
 
 	def short(self,url):
 		s = 'http://clck.ru/--?url=http%3A%2F%2Fwww.kolomna-kgpi.ru%2Findex2.php%3Foption%3Dcom_content%26task%3Dview%26id%3D'
 		fet = urllib2.urlopen(s+'%s' %url).read()
 		return fet
 
+	def gettitle(self,site):
+		page = urllib2.urlopen(site).read()
+		soup = BeautifulSoup(page)
+		soup.prettify()
+		title = soup.find(attrs={'class': 'contentheading'})
+		if title!=None:
+			title = title.text.encode('utf-8')
+			if len(title)>118:
+			 	title = title[:118-len(title)]
+		return title
+
+
 	def parse(self):
+		text = []
+		data = []
 		page = urllib2.urlopen(URL).read()
 		soup = BeautifulSoup(page)
 		soup.prettify()
@@ -55,16 +71,24 @@ class Tweetya(object):
 			 	title = title.text.encode('utf-8')
 			 	if len(title)>118:
 			 		title = title[:118-len(title)]
+			 	text.append(title)
 			if link!=None:
 				link = link.find(name = 'a').get('href')
 				link = link[70:-21]
-				self.setlink(link)
-				link = self.short(link[70:-21])
-				tweet = str(title)+' '+str(link)
-				print tweet
+				data.append(link)
+		return text,data
 
 
 
 if __name__ == '__main__':
+	reload(sys)
+	sys.setdefaultencoding('utf8')
 	t = Tweetya()
-	t.parse()
+	text,link = t.parse()
+	urls = t.getlinks()
+	for i in link:
+		if not (i in urls):
+			t.setlink(i)
+			short = t.short(i)
+			title = t.gettitle(short)
+			print title+' '+short  
