4e071511d3218340496dfdc9175d6246a4bfcc5a
Юрий Букаткин <yurij-bukatkin@yandex.ru>
2014-04-22 15:25:32 +0400

    Update Twittya.py

--- Twittya.py
+++ Twittya.py
@@ -3,13 +3,16 @@ import tweepy
 import codecs
 import urllib2
 import threading
+import MySQLdb
+import string
 from threading import BoundedSemaphore
 from BeautifulSoup import BeautifulSoup
+
 
 URL 		= "http://www.kolomna-kgpi.ru/index.php?option=com_content&task=blogcategory&"
 FILENAME 	= "tweets.txt"
 PAGES 		= ['id=1&Itemid=79','id=24&Itemid=78','id=22&Itemid=82','id=26&Itemid=83']
-MAXCONN		= 4
+MAXCONN		= 1
 
 CONSUMER_KEY 	= ''
 CONSUMER_SECRET = ''
@@ -48,23 +51,36 @@ class Tweetya(object):
 
 	
 	def getlinks(self):
-		links=[]
-		try:
-			handle = open(FILENAME,"r")
-			for line in handle.readlines():
-				links.append(line)
-		except:
-			print "Error read file"
-		handle.close
-		return links
+		db = MySQLdb.connect(host="localhost", user="root", passwd="пароль", db="contacts", charset='utf8')
+		cursor = db.cursor()
+		sql = """SELECT mail, name FROM eadres WHERE mail LIKE '%yandex.ru' LIMIT 10"""
+		cursor.execute(sql)
+		data =  cursor.fetchall()
+		db.close()
+		return data 
+		#links=[]
+		#try:
+		#	handle = open(FILENAME,"r")
+		#	for line in handle.readlines():
+		#		links.append(line)
+		#except:
+		#	print "Error read file"
+		#handle.close
+		#return links
 
 	def setlink(self,link):
-		try:
-			handle = open(FILENAME,'a')
-			handle.write(link);
-			handle.close
-		except:
-			print "Error read file"
+		db = MySQLdb.connect(host="localhost", user="root", passwd="пароль", db="contacts", charset='utf8')
+		cursor = db.cursor()
+		sql = """INSERT mail, name FROM eadres WHERE mail LIKE '%yandex.ru' LIMIT 10"""
+		cursor.execute(sql)
+		data =  cursor.fetchall()
+		db.close()
+		#try:
+		#	handle = open(FILENAME,'a')
+		#	handle.write(link);
+		#	handle.close
+		#except:
+		#	print "Error read file"
 
 	def short(self,url):
 		s = 'http://clck.ru/--?url=http%3A%2F%2Fwww.kolomna-kgpi.ru%2Findex2.php%3Foption%3Dcom_content%26task%3Dview%26id%3D'
